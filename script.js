fd6dfab0eb655998ba754cb885c0b0a56a32571f
michelangelomancini <michelangelomancini8@gmail.com>
2025-05-23 10:50:52 +0200

    versione migliorata getChefBirthday con log ricetta e chef, gestione errori chiamante

--- script.js
+++ script.js
@@ -3,20 +3,34 @@ async function getChefBirthday(id) {
     // chiamata per ottenere la ricetta specifica
     const recipes = await fetch(`https://dummyjson.com/recipes/${id}`);
     const dataRecipes = await recipes.json();
-    console.log(dataRecipes);
+
+    // mostro in console la ricetta
+    console.log("Nome ricetta:", dataRecipes.name);
+
 
     // chiamata per ottenere l'id dell'user 
     const userId = await fetch(`https://dummyjson.com/users/${dataRecipes.userId}`);
     const user = await userId.json();
 
-    // ritorno la data di nascita dell'user
+    // mostro in console il nome dello chef
+    console.log("Nome Chef:", user.firstName, user.lastName);
+
+
+    // ritorno la data di nascita dell'user (Output prncipale)
     return user.birthDate;
 
 
 }
 
+(async () => {
+    try {
 
-getChefBirthday(1)
-    .then(birthDate => {
-        console.log("Chef's Birthday:", birthDate);
-    });
+        const birthday = await getChefBirthday(4);
+        console.log("Data di nascita dello Chef:", birthday);
+
+    } catch (error) {
+
+        console.error("Errore:", error.message);
+    }
+})();
+
